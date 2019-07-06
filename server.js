6ebdc3d0bc361182a06f02e18b79d69af9af3a63
sanjaybora15 <sanjaybora15@gmail.com>
2019-07-06 23:00:29 +0530

    routes path added

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
-const connetDB = require('./configs/db');
+const connetDB = require('./config/db');
 const app = express();
 
 const port = process.env.port || 5000;
+
+//initialize database
 connetDB();
+
+//init  middleware
+app.use(express.json({ extended:false }));
+
 app.get('/',(req,res) =>{
-    res.send('API is running');
+    res.send('Home page is working perfectly');
 });
 
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-//the main file is coming
-//port iinitalized
-//hello
 
 app.listen(port, () => console.log(`server running at port no ${port}`))
